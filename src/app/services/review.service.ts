14507a875fca95c481b233e927aa695c655ee749
yupjpn <yuki.peters@duke.edu>
2019-06-02 18:41:01 +0100

    profile done

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -16,9 +16,13 @@ export class ReviewService {
   constructor(rentalService: RentalService) {
     this.reviews = new Array<Review>();
 
-    let review1 = new Review("John", rentalService.getRentals()[0], "Amazing place!", 4, "May 2019");
-    let review2 = new Review("Nick", rentalService.getRentals()[1], "Great place!", 5, "April 2019");
-    let review3 = new Review("Julian", rentalService.getRentals()[2], "Wonderful place!", 5, "January 2019");
+    let review1 = new Review("John", rentalService.getRentals()[0], 
+    "We had a great time at Yuki's place. The communication was super easy and quick and the place looks exactly like it does on the pictures. The neighborhood is very conveniently located and although it’s a lively neighborhood the apartment is very quiet! Would absolutely recommend to stay there!", 
+    4, "May 2019");
+    let review2 = new Review("Nick", rentalService.getRentals()[1], "Yuki's place is at a perfect location, very easy to get around with public transportation and walking distance to awesome bars and restaurants. His place is great, very clean and has everything you need. (Host Name) was also very helpful since the beginning, available at all times whenever I had any questions. Totally recommend it.", 
+    5, "April 2019");
+    let review3 = new Review("Julian", rentalService.getRentals()[2], "The apartment is very nice and comfortable! Location is very convenient, near shops and public transport. (Host Name) was very helpful and friendly, he’s a super host!",
+    5, "January 2019");
 
     this.reviews.push(review1);
     this.reviews.push(review2);
